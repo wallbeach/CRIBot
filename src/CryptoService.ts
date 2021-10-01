9916bc7a11b330d1829d0e42ed19e63c11844f41
wallbeach <thierry.thomann@gmail.com>
2021-10-01 15:38:45 +0200

    0.3

--- src/CryptoService.ts
+++ src/CryptoService.ts
@@ -1,4 +1,5 @@
 import crypto from "crypto-js";
+import axios from "axios";
 import { IAccount, IOrder, ITrade, ICoin, ITicker, IInstrument } from "./Interface";
 import { Config } from './Config.js';
 
